fe4173102a567d6d06a34d8bb05588ce0ac47902
Guilmar Ciriaco Soto <guilmar.ciriaco@unas.edu.pe>
2020-09-07 19:33:40 -0500

    Modulo Gestion-Habitaciones, Se ah creado la vista para mostrar los registros de habitaciones que tenemos en la base de datos, incluyendo la funcionalidad de agregar un nuevo registro para las habitaciones

---
+++ app/Http/Requests/StoreBedroomPost.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreBedroomPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'nro' => ['required', 'numeric'],
+            'nro_beds' => ['required', 'numeric'],
+            'size_beds' => ['required'],
+            'floor' => ['required', 'numeric'],
+            'is_bath' => ['required', 'boolean'],
+            'price' => ['required', 'numeric']
+
+        ];
+    }
+}
--- app/Model/Bedroom.php
+++ app/Model/Bedroom.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bedroom extends Model
 {
-    //
+    protected $table = 'bedrooms';
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nro', 'nro_beds', 'size_beds', 'floor', 'is_bath', 'price'
+    ];
+
 }
